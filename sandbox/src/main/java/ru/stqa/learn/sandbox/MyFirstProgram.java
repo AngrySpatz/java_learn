ab3b2cecc05256522d4e9befbb98080bba9f734c
Oleksandr Vorobei <aleksandr.vorobej@gmail.com>
2018-05-08 18:18:30 +0300

    доб неск функ

--- sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
@@ -4,7 +4,28 @@ public class MyFirstProgram {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("world");
+        hello("jopa");
+        double len = 5;
+        System.out.println("ololo 1 " + len + "=" + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("ololo 2 " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 
 }
